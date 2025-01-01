25aa6ce82e9fb78412d78005803955a393a8ed90
Efe Koc <efekoc1461@icloud.com>
2025-01-01 20:34:35 +0300

    Code updated.

---
+++ LivePolls/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  LivePolls
+//
+//  Created by Efe Koç on 31.12.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ LivePolls/Views/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  LivePolls
+//
+//  Created by Efe Koç on 31.12.2024.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
